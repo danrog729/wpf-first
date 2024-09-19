73f1df4033002178bde321d8a22e552d8fe91c70
Daniel Rogers <19RogersDan@student.bournemouth-school.org>
2024-09-19 09:16:43 +0100

    Add project files.

---
+++ wpf-first/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_first
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("clicked!");
+        }
+    }
+
+    public partial class LayoutWindow : Window
+    {
+        public LayoutWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
